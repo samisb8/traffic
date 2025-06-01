8a2ba8c3ed5c6e5dd3bd13f7d0523dec7ea3c4c2
samisb8 <157204225+samisb8@users.noreply.github.com>
2025-06-01 21:02:27 +0100

    Update train.py

--- mlops/train.py
+++ mlops/train.py
@@ -10,7 +10,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
-from streamlit import json
+
 
 
 def load_data():
